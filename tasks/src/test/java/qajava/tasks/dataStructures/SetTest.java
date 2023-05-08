703bbd68fcb6caa34d4aac85b3c890f2fdcd88d8
Branko Bavrljic <branko.bavrljic@createq.space>
2023-05-08 16:00:47 +0200

    Set tests and implementation

---
+++ tasks/src/test/java/qajava/tasks/dataStructures/SetTest.java
@@ -0,0 +1,119 @@
+package qajava.tasks.dataStructures;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+
+  final int TEST_VALUE_1 = 1;
+  final int TEST_VALUE_2 = 2;
+  final int NON_EXISTENT_VALUE = Integer.MAX_VALUE;
+
+  @Test
+  void add() {
+    //setup
+    Set set = new Set();
+
+    //operation under test
+    set.add(TEST_VALUE_1);
+
+    //test
+    assertTrue(set.contains(TEST_VALUE_1));
+  }
+
+  @Test
+  void add_with2elements() {
+    //setup
+    Set set = new Set();
+
+    //operation under test
+    set.add(TEST_VALUE_1);
+    set.add(TEST_VALUE_2);
+
+    //test
+    assertTrue(set.contains(TEST_VALUE_1));
+    assertTrue(set.contains(TEST_VALUE_2));
+  }
+
+  @Test
+  void add_confirmNoDuplicates() {
+    //setup
+    Set set = new Set();
+
+    //operation under test
+    set.add(TEST_VALUE_1);
+    set.add(TEST_VALUE_1);
+    set.remove(TEST_VALUE_1);
+
+
+    //test
+    assertFalse(set.contains(TEST_VALUE_1));
+  }
+
+  @Test
+  void add_confirmNoDuplicates_withTwoValues() {
+    //setup
+    Set set = new Set();
+
+    //operation under test
+    set.add(TEST_VALUE_1);
+    set.add(TEST_VALUE_2);
+    set.add(TEST_VALUE_1);
+    set.remove(TEST_VALUE_1);
+
+
+    //test
+    assertFalse(set.contains(TEST_VALUE_1));
+    assertTrue(set.contains(TEST_VALUE_2));
+  }
+
+  @Test
+  void remove() {
+    //setup
+    Set set = new Set();
+    set.add(TEST_VALUE_1);
+
+    //operation under test
+    set.remove(TEST_VALUE_1);
+
+    //test
+    assertFalse(set.contains(TEST_VALUE_1));
+  }
+
+  @Test
+  void remove_oneOfTwoValues() {
+    //setup
+    Set set = new Set();
+    set.add(TEST_VALUE_1);
+    set.add(TEST_VALUE_2);
+
+    //operation under test
+    set.remove(TEST_VALUE_1);
+
+    //test
+    assertFalse(set.contains(TEST_VALUE_1));
+    assertTrue(set.contains(TEST_VALUE_2));
+  }
+
+  @Test
+  void contains() {
+    Set set = new Set();
+    set.add(TEST_VALUE_1);
+
+    assertTrue(set.contains(TEST_VALUE_1));
+    assertFalse(set.contains(NON_EXISTENT_VALUE));
+  }
+
+  @Test
+  void contains_withTwoValues() {
+    Set set = new Set();
+    set.add(TEST_VALUE_1);
+    set.add(TEST_VALUE_2);
+
+    assertTrue(set.contains(TEST_VALUE_1));
+    assertTrue(set.contains(TEST_VALUE_2));
+    assertFalse(set.contains(NON_EXISTENT_VALUE));
+  }
+}
