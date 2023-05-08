a2a0f79715c2d8200ce4192f0ea08a9ae76f4e21
Branko Bavrljic <branko.bavrljic@createq.space>
2023-05-08 11:37:10 +0200

    simplest test passing code

--- tasks/src/main/java/qajava/tasks/dataStructures/SingleLinkedList.java
+++ tasks/src/main/java/qajava/tasks/dataStructures/SingleLinkedList.java
@@ -1,28 +1,31 @@
 package qajava.tasks.dataStructures;
 
 public class SingleLinkedList implements IntList {
+
+  private Integer value;
+
   @Override
   public void add(Integer value) {
-
+    this.value = value;
   }
 
   @Override
   public void remove(Integer value) {
-
+    this.value = null;
   }
 
   @Override
   public Boolean contains(Integer value) {
-    return null;
+    return this.value == value;
   }
 
   @Override
   public Integer get(Integer index) {
-    return null;
+    return this.value;
   }
 
   @Override
   public void addAtIndex(Integer index, Integer value) {
-
+    this.value = value;
   }
 }
